fae1bb3e49af7d12d25fe57e71d24830409852fb
Hussein Beshir <133501119+HussooB@users.noreply.github.com>
2026-08-20 02:21:43 +0300

    Update intials.py

--- intials.py
+++ intials.py
@@ -1,2 +1,5 @@
 
 print("Here We Go Again!")
+
+#learned that string as we know it mostly in variable so all the methods apply with variable then . and the method name with parenthesis but i learned that
+# u can do . and method name without variable straight from the string quotation
