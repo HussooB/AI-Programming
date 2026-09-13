352af353730ea1403404c509c8f80cbcc80d76ef
Hussein Beshir <133501119+HussooB@users.noreply.github.com>
2026-09-13 15:58:53 +0000

    starter

--- intials.py
+++ intials.py
@@ -3,3 +3,4 @@ print("Here We Go Again!")
 
 #learned that string as we know it mostly in variable so all the methods apply with variable then . and the method name with parenthesis but i learned that
 # u can do . and method name without variable straight from the string quotation
+#now we start coding from here the github workspace
